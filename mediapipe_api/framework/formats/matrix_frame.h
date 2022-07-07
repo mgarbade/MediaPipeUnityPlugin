ca5efe76935ac58110a86f9813d2a68b2e6e0f59
Martin Garbade <garbade.work@gmail.com>
2022-07-07 17:19:30 +0200

    change line endings: from CLRF -> LF

--- mediapipe_api/framework/formats/matrix_frame.h
+++ mediapipe_api/framework/formats/matrix_frame.h
@@ -15,8 +15,8 @@
 
 extern "C" {
 
-MP_CAPI(MpReturnCode) mp__MakeMatrixPacket__PKc_i(const char* matrix_data, int size, mediapipe::Packet** packet_out);
-MP_CAPI(MpReturnCode) mp__MakeMatrixPacket_At__PA_i_Rt(const char* matrix_data, int size, mediapipe::Timestamp* timestamp,
+MP_CAPI(MpReturnCode) mp__MakeMatrixPacket__PKc_i(const char* matrix_data_serialized, int size, mediapipe::Packet** packet_out);
+MP_CAPI(MpReturnCode) mp__MakeMatrixPacket_At__PA_i_Rt(const char* matrix_data_serialized, int size, mediapipe::Timestamp* timestamp,
                                                                mediapipe::Packet** packet_out);
 // MP_CAPI(MpReturnCode) mp_Packet__GetMatrix(mediapipe::Packet* packet, const mediapipe::Matrix** value_out);
 // MP_CAPI(void) mp_Matrix__delete(mediapipe::Matrix* matrix);
